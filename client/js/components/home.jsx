6e739f6faaba5a13bed6b5ce0933a6dabe5ad355
Justin Ball <justinball@gmail.com>
2016-01-13 13:27:59 -0700

    buttons and inputs

--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -3,14 +3,29 @@
 import React                    from 'react';
 import assets                   from '../libs/assets';
 
+
 class Home extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      text: "Initial text"
+    };
+  }
+
+  clicked(){
+    this.setState({ text: this.refs.textBox.value });
+  }
 
   render(){
     
     const img = assets("./images/atomicjolt.jpg");
 
-    return<div>
+    return <div>
+      { this.state.text }
       <img src={img} />
+      <input ref="textBox" type="text" />
+      <button onClick={ (e) => { this.clicked(); } }>TheButton</button>
     </div>;
   }
 
